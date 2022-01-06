fabb03a36ddc5b166022bfa3990a7d42f9d756d4
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-01-06 23:26:09 +0300

    Complete react app with Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { About, Contact, Event, Home } from "./Pages";
+import {
+  About,
+  CompanyHistory,
+  Contact,
+  Event,
+  Home,
+  Location,
+  Services,
+  Whoops404,
+} from "./Pages";
 import App1 from "./App1";
 
 function App() {
@@ -9,10 +18,15 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<CompanyHistory />} />
+          <Route path="location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Event />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/learning" element={<App1 />} />
+        <Route path="*" element={<Whoops404 />} />
       </Routes>
     </div>
   );
--- src/Pages.js
+++ src/Pages.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, Outlet } from "react-router-dom";
 import "./App.css";
+import errorImage from "./404.png";
 
 export function Home() {
   return (
@@ -41,9 +42,23 @@ export function About() {
       <Link to="../contact">
         <button>Contact</button>
       </Link>
-      <Link to="learning">
+      <Link to="../learning">
         <button>App Learning</button>
       </Link>
+
+      <hr />
+
+      <Link to="services">
+        <button>Services</button>
+      </Link>
+      <Link to="history">
+        <button>Company History</button>
+      </Link>
+      <Link to="location">
+        <button>Our Location</button>
+      </Link>
+      <hr />
+      <Outlet />
     </div>
   );
 }
@@ -64,7 +79,7 @@ export function Event() {
       <Link to="../contact">
         <button>Contact</button>
       </Link>
-      <Link to="learning">
+      <Link to="../learning">
         <button>App Learning</button>
       </Link>
     </div>
@@ -87,9 +102,46 @@ export function Contact() {
       <Link to="">
         <button>Contact</button>
       </Link>
-      <Link to="learning">
+      <Link to="../learning">
         <button>App Learning</button>
       </Link>
+    </div>
+  );
+}
+
+export function Whoops404() {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <h1>Resource not found !</h1>
+      <h3>{location.pathname}</h3>
+
+      <img src={errorImage} alt="Error notice" />
+    </div>
+  );
+}
+
+export function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+    </div>
+  );
+}
+
+export function CompanyHistory() {
+  return (
+    <div>
+      <h1>Our Company History</h1>
+    </div>
+  );
+}
+
+export function Location() {
+  return (
+    <div>
+      <h1>Our Location</h1>
     </div>
   );
 }
